6bbe23956bcf5e30d432ceefa826bd1b8293d6d3
junaid059 <junaidkhalid2001@gmail.com>
2026-02-14 16:35:04 +0500

    schema

--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field 
 
 
 class UserBase(BaseModel):
     email: str
-    password: str
+    password: str = Field(..., min_length=3, max_length=128)
 
 class UserCreate(UserBase):
     pass
